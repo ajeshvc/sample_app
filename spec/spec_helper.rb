b9894ce2d4d3e7af1d26545014644f1085cddb58
Ajesh <ajesh@helloinfinity.com>
2013-08-26 15:28:34 +0530

    Finish static Pages2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,3 @@
-
-RSpec.configure do |config|
-
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -43,9 +39,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-end
-
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-
   config.include Capybara::DSL
 end
+
